523bac01e902fff9a3cfddd26c91672a3cdaf495
caio <caioazbonomo@gmail.com>
2025-05-28 21:52:43 -0300

    Adicionado o Stack navigation.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./screens/Home";
+import Empresa from "./screens/Empresa";
+import Operador from "./screens/Operador";
+import TipoUsuario from "./screens/TipoUsuario";
+import Passageiro from "./screens/Passageiro";
+import Motorista from "./screens/Motorista";
+
+export type RootStackParamList = {
+  Home: undefined;
+  Empresa: undefined;
+  Operador: undefined;
+  TipoUsuario: undefined;
+  Passageiro: undefined;
+  Motorista: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Empresa" component={Empresa} />
+        <Stack.Screen name="Operador" component={Operador} />
+        <Stack.Screen name="TipoUsuario" component={TipoUsuario} />
+        <Stack.Screen name="Passageiro" component={Passageiro} />
+        <Stack.Screen name="Motorista" component={Motorista} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
